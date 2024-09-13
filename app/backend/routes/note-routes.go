51e9afdfe5b0a287ecf692f5c3a5a63dbf24d5e5
Matt Wang <matthewianwang@gmail.com>
2024-09-13 14:42:45 -0700

    Updated routes to utilize middleware

--- app/backend/routes/note-routes.go
+++ app/backend/routes/note-routes.go
@@ -3,11 +3,13 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/matthewwangg/papertrail-backend/controllers"
+	"github.com/matthewwangg/papertrail-backend/middleware"
 )
 
 // SetupNoteRoutes configures the note routes
 func SetupNoteRoutes(router *gin.Engine) {
 	notes := router.Group("/notes")
+	notes.Use(middleware.AuthMiddleware())
 	{
 		notes.GET("", controllers.GetNotes)
 		notes.GET("/:id", controllers.GetNoteByID)
