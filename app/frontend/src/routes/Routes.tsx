deafb04a62e59eb27c006f2d45164f741c3102f5
Matt Wang <matthewianwang@gmail.com>
2024-09-17 13:58:41 -0700

    Added protected routes to app routes

--- app/frontend/src/routes/Routes.tsx
+++ app/frontend/src/routes/Routes.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage';
 import SignupPage from '../pages/SignUpPage';
+import ProtectedRoute from "./ProtectedRoutes";
+import NotesPage from "../pages/NotesPage";
+import TasksPage from "../pages/TasksPage";
 
 const AppRoutes: React.FC = () => {
     return (
@@ -10,6 +13,24 @@ const AppRoutes: React.FC = () => {
                 {/* Public Routes */}
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/signup" element={<SignupPage />} />
+
+                {/* Protected Routes */}
+                <Route
+                    path="/notes"
+                    element={
+                        <ProtectedRoute>
+                            <NotesPage />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/tasks"
+                    element={
+                        <ProtectedRoute>
+                            <TasksPage />
+                        </ProtectedRoute>
+                    }
+                />
 
                 {/* Default Route */}
                 <Route path="*" element={<LoginPage />} />
