156aad90351b8d18f467c2525644cf4fd479230f
Matt Wang <matthewianwang@gmail.com>
2025-01-05 04:55:48 -0700

    Cleaned up old UI provider

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AppRoutes from "./routes/Routes";
-import { NextUIProvider } from '@nextui-org/react';
 
 function App() {
   return (
     <div className="App">
-        <NextUIProvider>
-            <AppRoutes/>
-        </NextUIProvider>
+        <AppRoutes/>
     </div>
   );
 }
