5859935d53a6e50b7d9cd8f972020e8382a6f3ae
Matt Wang <matthewianwang@gmail.com>
2025-01-06 23:10:08 -0700

    Added the token expiry time check

--- app/frontend/src/routes/ProtectedRoutes.tsx
+++ app/frontend/src/routes/ProtectedRoutes.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, {JSX} from 'react';
 import { Navigate } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
+
+interface JwtPayload {
+    exp: number;
+}
 
 interface ProtectedRouteProps {
     children: JSX.Element;
 }
 
+const isTokenValid = (token: string): boolean => {
+    try {
+        const decodedToken = jwtDecode<JwtPayload>(token);
+        const { exp } = decodedToken;
+
+        return exp * 1000 > Date.now();
+    } catch (error) {
+        console.error('Error decoding token:', error);
+        return false;
+    }
+};
+
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
     const token = localStorage.getItem('token');
-    
-    // Validate the token's expiration here
 
-    return token ? children : <Navigate to="/login" replace />;
+    if (token && isTokenValid(token)) {
+        return children;
+    } else {
+        return <Navigate to="/login" replace />;
+    }
 };
 
 export default ProtectedRoute;
