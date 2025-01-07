eb013d9f376de57656067a7f23720777c951afdb
Matt Wang <matthewianwang@gmail.com>
2025-01-06 23:37:10 -0700

    Updated user model to have refresh token

--- app/backend/internal/models/user.go
+++ app/backend/internal/models/user.go
@@ -6,7 +6,8 @@ import (
 
 type User struct {
 	gorm.Model
-	Username string `gorm:"unique" json:"username" validate:"required,min=3,max=20"`
-	Password string `json:"-" validate:"required,min=8"`
-	Notes    []Note `json:"notes"`
+	Username     string `gorm:"unique" json:"username" validate:"required,min=3,max=20"`
+	Password     string `json:"-" validate:"required,min=8"`
+	RefreshToken string `json:"-"`
+	Notes        []Note `json:"notes"`
 }
